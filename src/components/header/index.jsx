e27bbe607fdeba5b445aa6d88141e66f4271d887
JulSarmiento <jasa1999@hotmail.com>
2022-08-31 20:30:45 -0500

    leve avance en el header

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -33,7 +33,7 @@ export default function Header() {
             </div>
             <ul className="MENU-LINK-MOBILE-OPEN flex flex-col items-center justify-between min-h-[250px]">
               <li className="border-b border-gray-400 my-8 uppercase">
-                <Link to="/">Modo Nocturno</Link>
+                <Link to="/">Modo Diurno</Link>
               </li>
               <li className="border-b border-gray-400 my-8 uppercase">
                 <Link to='/Favourites'>Favoritos</Link>
